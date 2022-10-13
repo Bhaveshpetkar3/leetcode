084e02e851b22d8db53b634dab3dbd6e5c729eb4
Bhaveshpetkar3 <77126230+Bhaveshpetkar3@users.noreply.github.com>
2022-10-14 01:14:30 +0530

    nothing

--- 27-remove-element/27-remove-element.cpp
+++ 27-remove-element/27-remove-element.cpp
@@ -11,3 +11,15 @@ public:
     return nums.size()-cnt;   
     }
 };
+//alternate
+// class Solution {
+// public:
+//     int removeElement(vector<int>& nums, int val) {
+//         int ptr = 0;
+        
+//         for (int i = 0; i < nums.size(); i++)
+//             if (nums[i] != val) nums[ptr++] = nums[i];
+        
+//         return ptr;
+//     }
+// };
