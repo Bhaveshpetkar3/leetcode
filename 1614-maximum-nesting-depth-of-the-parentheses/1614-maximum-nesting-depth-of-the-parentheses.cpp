8ca6bc5948fdce32424ea27c7a88a15772896cdc
Bhaveshpetkar3 <77126230+Bhaveshpetkar3@users.noreply.github.com>
2022-10-20 22:33:39 +0530

    Time: 4 ms (35.18%), Space: 6.3 MB (57.55%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -6,19 +6,10 @@ public:
         
          for(int i=0;i<len;i++)
          {
-             if(s[i]=='(')
-             {
-                 ctr++;
-                 maxi=max(ctr,maxi);
-             }
-             else if(s[i]==')')
-             {
-                 
-                 ctr--;
-             }
-             cout<<maxi<<" ";
+             if(s[i]=='(')ctr++;
+             else if(s[i]==')')ctr--;
+             maxi=max(ctr,maxi);
          }
-        
-        return maxi;
+     return maxi;
     }
 };
