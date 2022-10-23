36a030e0495473a25b032e982c978b99b6b211e2
Bhaveshpetkar3 <77126230+Bhaveshpetkar3@users.noreply.github.com>
2022-10-24 04:14:17 +0530

    Time: 71 ms (51.42%), Space: 26 MB (53.53%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,52 +1,12 @@
 class Solution {
 public:
-    vector<int> sortedSquares(vector<int>& nums) {
-        vector <int> ans;
-        if(nums.size()==1)
-        {
-            ans.push_back(pow(nums[0],2));
-            return ans;
+    vector<int> sortedSquares(vector<int>& A) {
+        vector<int> res(A.size());
+        int l = 0, r = A.size() - 1;
+        for (int k = A.size() - 1; k >= 0; k--) {
+            if (abs(A[r]) > abs(A[l])) res[k] = A[r] * A[r--];
+            else res[k] = A[l] * A[l++];
         }
-        int ptr=-1,i=0;
-        if(nums[0]<0)
-        {
-            ptr++;
-            while(ptr<nums.size() && nums[ptr]<0 )ptr++;  
-            i=ptr;
-            ptr-=1;
-        }
-        while(ptr>=0 && i<nums.size())
-        {
-            if(nums[i]<abs(nums[ptr]))
-            {
-                ans.push_back(pow(nums[i],2));
-                i++;
-            }
-            else
-            {
-                ans.push_back(pow(nums[ptr],2));
-                ptr--;
-            }
-        }
-        if(i<nums.size() || ptr>=0)
-        {
-            if(i<nums.size())
-            {
-                while(i<nums.size())
-                {
-                    ans.push_back(pow(nums[i],2));
-                    i++;
-                }
-            }
-            else
-            {
-                while(ptr>=0)
-                {
-                     ans.push_back(pow(nums[ptr],2));
-                     ptr--;
-                }
-            }
-        }
-        return ans;
+        return res;
     }
 };
