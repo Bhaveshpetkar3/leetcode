944a01f3f57e136392ceaedbbca527810bd0611c
Bhaveshpetkar3 <77126230+Bhaveshpetkar3@users.noreply.github.com>
2022-10-25 18:13:50 +0530

    Time: 4 ms (75.96%), Space: 7.3 MB (88.34%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.cpp
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -6,8 +6,11 @@ public:
         int i=1;
         for(int j=0;j<n;j++)
         {
-            if(i%3==0 && i%5==0)ans[j]="FizzBuzz";
-            else if(i%3==0)ans[j]="Fizz";
+            if(i%3==0)
+            {
+                if(i%5==0)ans[j]="FizzBuzz";
+                else ans[j]="Fizz";
+            }
             else if(i%5==0)ans[j]="Buzz";
             else ans[j]=to_string(i);
             i++;
