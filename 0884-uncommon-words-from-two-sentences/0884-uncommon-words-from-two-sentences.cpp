2b654a92ced1fc4e93951edb6c51db36aca7e5b5
Bhaveshpetkar3 <77126230+Bhaveshpetkar3@users.noreply.github.com>
2022-10-27 01:14:29 +0530

    Time: 7 ms (29.02%), Space: 7 MB (19.21%) - LeetHub

---
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<string> uncommonFromSentences(string s1, string s2) {
+        map <string,int> mp1;
+        map <string,int> mp2;
+        vector <string> ans;
+        string a="";
+        for(int i=0;i<s1.size();i++)
+        {
+            if(s1[i]==' ')
+            {
+                mp1[a]++;
+                a="";
+                continue;
+            }
+            a+=s1[i];
+            if(i==s1.size()-1)mp1[a]++;
+            
+        }
+        a="";
+        for(int i=0;i<s2.size();i++)
+        {
+            if(s2[i]==' ')
+            {
+                mp2[a]++;
+                a="";
+                continue;
+            }
+            a+=s2[i];
+             if(i==s2.size()-1)mp2[a]++;
+        }
+        //cout<<mp2["sour"];
+        for(auto i:mp1)
+        {
+           if(i.second==1 && mp2[i.first]==0)ans.push_back(i.first);
+        }
+         for(auto i:mp2)
+        {
+            if(i.second==1 && mp1[i.first]==0)ans.push_back(i.first);
+        }
+        return ans;
+    }
+};
