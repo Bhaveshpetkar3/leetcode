cb12c81e33f9b47d638470f135cf632623b128b8
Bhaveshpetkar3 <77126230+Bhaveshpetkar3@users.noreply.github.com>
2022-10-26 20:09:56 +0530

    Time: 43 ms (22.25%), Space: 15.9 MB (71.36%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -7,11 +7,10 @@ public:
         while(left<=right)
         {
             mid=(left+right)/2;
-            if(letters[mid]==target)left=mid+1;
-            else if(letters[mid]<target)left=mid+1;
+            if(letters[mid]<=target)left=mid+1;
             else right=mid-1;
         }
-        if(left>=letters.size())return letters[0];
+        if(left>=letters.size())return letters[0];//In case if left counter exceeds the size of array 
         return letters[left];
     }
 };
