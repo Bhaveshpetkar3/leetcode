ddc1ac6e5e06de929bf140ff348893bc4942874c
Bhaveshpetkar3 <77126230+Bhaveshpetkar3@users.noreply.github.com>
2022-10-30 16:39:02 +0530

    Time: 5 ms (15.45%), Space: 5.9 MB (46.31%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -1,25 +1,14 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-        int ans=3;
-        int x=num;
-        bool flag=false;
-        int pos=-1;
-        int pos1=-1;
-        while(x)
+        string s=to_string(num);
+        for(int i=0;i<s.size();i++)
         {
-            pos++;
-            if(x%10==6)
-            {
-                pos1=pos;
-                flag=true;
-            }
-            
-            x/=10;
+            if(s[i]=='6')
+            { s[i]='9';
+                break;}
         }
-        //pos++;
-        cout<<pos<<" "<<pos1;
-        if(flag)return (num+(3*pow(10,pos1)));
-        return num;
+        int ans=stoi(s);
+        return ans;
     }
 };
