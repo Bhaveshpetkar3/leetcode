a93d2afac7988361c43a668501105d33a877aef9
Bhaveshpetkar3 <77126230+Bhaveshpetkar3@users.noreply.github.com>
2022-11-18 22:26:39 +0530

    Time: 270 ms (31.18%), Space: 52.2 MB (17.86%) - LeetHub

--- 0888-fair-candy-swap/0888-fair-candy-swap.cpp
+++ 0888-fair-candy-swap/0888-fair-candy-swap.cpp
@@ -2,12 +2,10 @@ class Solution {
 public:
     vector<int> fairCandySwap(vector<int>& aliceSizes, vector<int>& bobSizes) {
         int sumA=0,sumB=0;
-        map <int,int> mppA;
-        map <int,int> mppB;
+        unordered_map <int,int> mppB;
         for(auto i: aliceSizes)
         {
             sumA+=i;
-            mppA[i]++;
         }
         for(auto j: bobSizes)
         {
