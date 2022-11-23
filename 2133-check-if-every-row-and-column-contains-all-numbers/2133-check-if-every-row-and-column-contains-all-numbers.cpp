a8472e8279f18f082b37be11aa939f9b5ab3cb92
Bhaveshpetkar3 <77126230+Bhaveshpetkar3@users.noreply.github.com>
2022-11-23 20:12:46 +0530

    Time: 316 ms (68.86%), Space: 68.2 MB (51.65%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
@@ -2,21 +2,19 @@ class Solution {
 public:
     bool checkValid(vector<vector<int>>& matrix) {
         unordered_map <int,int> mp1;
+        unordered_map <int,int> mp2;
         int n=matrix.size();
         for(int i=0;i<matrix.size();i++)
         {
             for(int j=0;j<matrix[0].size();j++)
             {
                 mp1[matrix[i][j]]++;
+                mp2[matrix[j][i]]++;
             }
             if(mp1.size()!=n)return false;
+            if(mp2.size()!=n)return false;
             mp1.clear();
-            for(int j=0;j<matrix.size();j++)
-            {
-                mp1[matrix[j][i]]++;
-            }
-            if(mp1.size()!=n)return false;
-            mp1.clear();
+            mp2.clear();
         }
         return true;
     }
