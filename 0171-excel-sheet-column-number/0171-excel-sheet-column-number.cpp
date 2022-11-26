c012539a22fcb874def8dfdb9d24d07b38f266d2
Bhaveshpetkar3 <77126230+Bhaveshpetkar3@users.noreply.github.com>
2022-11-26 19:12:40 +0530

    Time: 3 ms (70.70%), Space: 6.3 MB (10.54%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     int titleToNumber(string s) {
-        int n=s.size();
-        n--;
-        //cout<<n;
+        int n=s.size()-1;
         int ans=0;
         for(int i=0;i<s.size();i++)
         {
-            cout<<s[i]-'A'+1<<" ";
-            ans+=(int)(pow(26,n)*(s[i]-'A'+1));
-                //ans+=(int)(pow((s[i]-'A'+1),n));
-            cout<<ans<<" ";
-                n--;
+            ans+=pow(26,n--)*(s[i]-'A'+1);
         }
         return ans;
     }
