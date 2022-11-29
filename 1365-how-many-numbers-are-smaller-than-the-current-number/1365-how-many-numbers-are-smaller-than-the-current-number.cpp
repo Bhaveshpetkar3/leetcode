886d3a69273e110935531b292f39cf33fdb2059d
Bhaveshpetkar3 <77126230+Bhaveshpetkar3@users.noreply.github.com>
2022-11-29 16:10:26 +0530

    Time: 16 ms (72.62%), Space: 10.5 MB (21.35%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -3,17 +3,17 @@ public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
         vector<int> arr=nums;
         vector <int> ans;
+        unordered_map <int,int> mpp;
         sort(arr.begin(),arr.end());
-        for(auto i: nums)
+        for(int i=nums.size()-1;i>=0;i--)
+            mpp[arr[i]]=i;
+        for(int i=0;i<nums.size();i++)
         {
-            int j=0;
-            for(auto h : arr)
-            {
-                if(i<h)continue;
-                if(i>h)j++;
-            }
-            ans.push_back(j);
+            nums[i]=mpp[nums[i]];
         }
-        return ans;
+        return nums;
     }
 };
+// for(int i=nums.size()-1;i>=0;i--)
+// Why you used the dec. loop ?
+//ANS: Because if he didnt run the loop in decreasing order, for same elements, while storing in hashmap, it will update to a higher index instead of first occurrence of the elements.
