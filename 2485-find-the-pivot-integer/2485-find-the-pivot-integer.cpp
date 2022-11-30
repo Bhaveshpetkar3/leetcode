9aafd5b90076259c7f165633d60d3817e0daaca7
Bhaveshpetkar3 <77126230+Bhaveshpetkar3@users.noreply.github.com>
2022-11-30 14:00:19 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (76.08%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,17 +1,10 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-        int tsum=(n*(n+1))/2;
-        vector <int> runsum;
-        int j=0;
-        if(n==1)return 1;
-        runsum.push_back(1);
-        for(int i=1;i<n;i++)
-        {
-            runsum.push_back((i+1)+runsum[i-1]);
-            if(runsum[i]==tsum-runsum[i-1])return i+1;
-        }
-        for(auto i : runsum)cout<<i<<" ";
-        return -1;
+        int ans = (n * n + n ) /2;
+        int sq = sqrt(ans);
+        if(sq * sq == ans)return sq;
+        else return -1;
     }
 };
+//https://leetcode.com/problems/find-the-pivot-integer/discuss/2851954/Simple-Maths-proof-with-picture
