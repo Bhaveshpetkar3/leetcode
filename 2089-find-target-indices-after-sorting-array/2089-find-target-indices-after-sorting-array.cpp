7c377d7e06746d0376b14ea2389f5f516ff2e980
Bhaveshpetkar3 <77126230+Bhaveshpetkar3@users.noreply.github.com>
2022-11-30 14:23:35 +0530

    Time: 8 ms (71.12%), Space: 11.5 MB (98.41%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -1,15 +1,17 @@
-//with sorting
+//without sorting
 class Solution {
 public:
     vector<int> targetIndices(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end());
-        int j=0;
         vector <int> ans;
+        int minele=0,targetcount=0,q=0;
         for(auto i:nums)
         {
-            if(i>target)break;
-            if(i==target)ans.push_back(j);
-            j++;
+            if(i<target)minele++;
+            if(i==target)targetcount++;
+        }
+        while(q<targetcount)
+        {
+            ans.push_back(minele+q++);
         }
         return ans;
     }
