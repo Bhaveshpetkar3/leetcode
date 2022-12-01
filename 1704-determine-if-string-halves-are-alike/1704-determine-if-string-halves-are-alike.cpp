73df586fb3c4ffdc01b0ac198c73a0aa5ab176ad
Bhaveshpetkar3 <77126230+Bhaveshpetkar3@users.noreply.github.com>
2022-12-01 12:58:26 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (7.65%) - LeetHub

---
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool halvesAreAlike(string s) {
+        unordered_map<char,int> mpp;
+        int ctr1=0,ctr2=0;
+        mpp['a']=1;
+        mpp['e']=1;
+        mpp['i']=1;
+        mpp['o']=1;
+        mpp['u']=1;
+        mpp['A']=1;
+        mpp['E']=1;
+        mpp['I']=1;
+        mpp['O']=1;
+        mpp['U']=1;
+        for(int i=0;i<s.size()/2;i++)
+        {
+            if(mpp[s[i]]==1)ctr1++;
+            if(mpp[s[i+s.size()/2]]==1)ctr2++;
+        }
+    return ctr1==ctr2;
+    }
+};
