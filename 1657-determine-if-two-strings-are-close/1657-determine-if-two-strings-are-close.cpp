a9631442d4e08de5806302b3a1ac8407851e638d
Bhaveshpetkar3 <77126230+Bhaveshpetkar3@users.noreply.github.com>
2022-12-02 16:31:10 +0530

    Time: 356 ms (17.96%), Space: 20.8 MB (16.77%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     bool closeStrings(string w1, string w2) {
         if(w1.size()!=w2.size())return false;
-        map<char,int> mp1;
-        map<char,int> mp2;
+        map<char,int> mp1,mp2;
         for(int i=0;i<w1.size();i++)
         {
             mp1[w1[i]]++;
@@ -16,8 +15,7 @@ public:
             else return false;
             
         }
-        map<int,int> mp3;
-        map<int,int> mp4;
+        map<int,int> mp3,mp4;
         for(auto i:mp1)mp3[i.second]++;
         for(auto i:mp2)mp4[i.second]++;
         if(mp3==mp4)return true;
