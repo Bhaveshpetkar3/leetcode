f7560bea24e5663bcf784c8c7e2efa09a0c51ce9
Bhaveshpetkar3 <77126230+Bhaveshpetkar3@users.noreply.github.com>
2022-12-03 13:12:12 +0530

    Time: 29 ms (45.53%), Space: 8.3 MB (85.65%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -5,7 +5,7 @@ public:
         unordered_map<char,int> mp1;
         for(auto i: s)mp1[i]++;
         vector<pair<int,char>> q;
-        for(auto i: mp1)q.push_back(make_pair(i.second,i.first));
+        for(auto i: mp1)q.push_back({i.second,i.first});
         sort(q.begin(),q.end(),greater<>());
         for(auto i:q)while(i.first--)ans+=i.second;
         return ans;
