202da6e8870bfd34b7f471d4cbd96259ce29a7a4
Bhaveshpetkar3 <77126230+Bhaveshpetkar3@users.noreply.github.com>
2022-12-03 13:53:16 +0530

    Time: 81 ms (5.01%), Space: 7.7 MB (5.68%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int h=2;
+        string a="1",b="1";
+        //int a=0;
+        map<char,int> mp;
+        mp[s[0]]=1;
+        for(int i=1;i<s.size();i++)
+        {
+            if(mp[s[i]])a+=to_string(mp[s[i]]);
+            else
+            {
+                mp[s[i]]=h++;
+                a+=to_string(mp[s[i]]);
+            }
+        }
+        //cout<<a<<" ";
+        mp.clear();
+        h=2;
+        mp[t[0]]=1;
+        for(int i=1;i<t.size();i++)
+        {
+            if(mp[t[i]])b+=to_string(mp[t[i]]);
+            else
+            {
+                mp[t[i]]=h++;
+                b+=to_string(mp[t[i]]);
+            }
+        }
+        // cout<<a<<" "<<b;
+        return a==b;
+        
+    }
+};
