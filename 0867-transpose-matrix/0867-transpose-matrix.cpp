6a23ab6c5a42f779824502dc1a4555d8cb09000d
Bhaveshpetkar3 <77126230+Bhaveshpetkar3@users.noreply.github.com>
2022-12-05 20:02:08 +0530

    Time: 28 ms (47.79%), Space: 10.5 MB (78.61%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.cpp
+++ 0867-transpose-matrix/0867-transpose-matrix.cpp
@@ -5,10 +5,7 @@ public:
         vector<vector<int>> ans(n,vector<int>(m));
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<m;j++)
-            {
-                ans[i][j]=matrix[j][i];
-            }
+            for(int j=0;j<m;j++)ans[i][j]=matrix[j][i];
         }
         return ans;
     }
