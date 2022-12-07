bac014d5b2eab303a4baa7fe861b09a8ff03935c
Bhaveshpetkar3 <77126230+Bhaveshpetkar3@users.noreply.github.com>
2022-12-07 19:16:53 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (27.45%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n==0)return false;
-        return (ceil(log2(n)) == floor(log2(n)));
+      bool ans= n==0 ? false : (ceil(log2(n)) == floor(log2(n)));
+      return ans;
     }
 };
