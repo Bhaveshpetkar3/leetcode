94e4314d63ba98b68cd384b5e0e5803a2e3bfa08
Bhaveshpetkar3 <77126230+Bhaveshpetkar3@users.noreply.github.com>
2022-12-08 11:47:07 +0530

    Time: 8 ms (42.48%), Space: 12.9 MB (10.23%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
@@ -4,29 +4,29 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-void postorder(TreeNode *root,vector<int> &qwe)
+void postorder(TreeNode *root,string &s)
     {
         if(root)
         {
-            if(root->left==NULL && root->right==NULL)qwe.push_back(root->val);
-        postorder(root->left,qwe);
-        postorder(root->right,qwe);
+            if(root->left==NULL && root->right==NULL)s+=to_string(root->val)+"->";
+        postorder(root->left,s);
+        postorder(root->right,s);
         
         }
     return;
     }
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-        vector <int> ans1;
-        vector <int> ans2;
-        postorder(root1,ans1);
-        postorder(root2,ans2);
-        return ans1==ans2;
+        string s1="",s2="";
+        postorder(root1,s1);
+        postorder(root2,s2);
+        cout<<s1<<" "<<s2;
+        return s1==s2;
     }
 };
