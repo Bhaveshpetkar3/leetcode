e8f7bfc0733d03004319b4ce64c71a1a07158ce5
Bhaveshpetkar3 <77126230+Bhaveshpetkar3@users.noreply.github.com>
2023-02-06 14:58:18 +0530

    Time: 0 ms (100.00%), Space: 6 MB (52.18%) - LeetHub

--- 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.cpp
+++ 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     bool squareIsWhite(string c) {
-        int a=c[0];
-        int b=c[1];
-        if((a%2!=0 && b%2!=0)|| (a%2==0 && b%2==0))return false;
+        if((c[0]%2!=0 && c[1]%2!=0)|| (c[0]%2==0 && c[1]%2==0))return false;
         return true;
     }
 };
