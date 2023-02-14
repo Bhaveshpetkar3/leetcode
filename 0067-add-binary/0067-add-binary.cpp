f78e67f193be06bbf3f2e6d1eb71e989bc3054f6
Bhaveshpetkar3 <77126230+Bhaveshpetkar3@users.noreply.github.com>
2023-02-14 22:48:13 +0530

    Time: 2 ms (73.96%), Space: 6.4 MB (61.65%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -5,10 +5,10 @@ public:
         int j=b.length()-1;
         int c=0;
         string ans="";
-        while(i>=0 || j>=0 || c==1)
+        while(i>=0 || j>=0 || c!=0)
         {
-            c+=i>=0?a[i--]-'0':0;
-            c+=j>=0?b[j--]-'0':0;
+            c+=(i>=0)?a[i--]-'0':0;
+            c+=(j>=0)?b[j--]-'0':0;
             ans+=((c%2)+ '0');
             c=c>>1;
         }
