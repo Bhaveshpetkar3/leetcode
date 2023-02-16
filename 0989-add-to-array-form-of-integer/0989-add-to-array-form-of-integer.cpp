c85d271ff07d5d1b37201c7185aaecb5e2cc272c
Bhaveshpetkar3 <77126230+Bhaveshpetkar3@users.noreply.github.com>
2023-02-16 10:53:07 +0530

    Time: 26 ms (87.90%), Space: 28.7 MB (33.51%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.cpp
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.cpp
@@ -5,8 +5,8 @@ public:
         vector <int> ans;
         while(i>=0 || carry || k)
         {
-            if(i>=0)j=k?carry+num[i--]+k%10:carry+num[i--];
-            else j=k?carry+k%10:carry;
+            if(i>=0)j=carry+num[i--]+k%10;
+            else j=carry+k%10;
             ans.push_back(j%10);
             carry=j/10;
             k/=10;
