0b6ed0b8b3f3c9241dace60f329f225b3c9fd0b3
Bhaveshpetkar3 <77126230+Bhaveshpetkar3@users.noreply.github.com>
2023-02-22 19:25:33 +0530

    Time: 71 ms (47.95%), Space: 26.2 MB (37.41%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -3,11 +3,12 @@ public:
     int shipWithinDays(vector<int>& weights, int days) {
         int l=0,r=999999;
         int mid;bool flag;
-        while(r-1>l){
+        while(r>l+1){
             mid=(l+r)/2;
             flag=1;
             int n=1,Size=0;
-            for(int i=0;i<weights.size();i++){
+            int i=0;
+            while(i<weights.size()){
                 if(weights[i]>mid)
                 {
                     flag=0;
@@ -19,6 +20,7 @@ public:
                     Size=weights[i];
                     n++;
                 }
+                i++;
             }
             if(n>days) flag=false;
             if(flag) r=mid;
