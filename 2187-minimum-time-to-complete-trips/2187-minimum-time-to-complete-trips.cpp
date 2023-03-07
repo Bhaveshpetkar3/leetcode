e7a055d770c7a2d08d29435eeaf959185ca47360
Bhaveshpetkar3 <77126230+Bhaveshpetkar3@users.noreply.github.com>
2023-03-07 13:20:11 +0530

    Time: 517 ms (29.83%), Space: 94.6 MB (33.40%) - LeetHub

---
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long minimumTime(vector<int>& time, int totalTrips) {
+        long long ans=0,low=1,high=99999999999999,mid;
+        long long i=1;
+        sort(time.begin(),time.end());
+        while(low<=high)
+        {
+            mid=low+(high-low)/2;
+            ans=0;
+            for(auto j:time)ans+=(mid/j);
+            if(ans>=totalTrips)high=mid-1;
+            else low=mid+1;
+        }
+        return low;
+    }
+};
