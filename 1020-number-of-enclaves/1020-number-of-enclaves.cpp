b026af859130e0a8479edbe48886407bb8ab4fa5
Bhaveshpetkar3 <77126230+Bhaveshpetkar3@users.noreply.github.com>
2023-03-15 22:15:43 +0530

    Time: 55 ms (99.78%), Space: 33.1 MB (27.37%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -7,8 +7,6 @@ public:
         vis[row][col]=1;
         int dx[]={-1,0,1,0};
         int dy[]={0,-1,0,1};
-        queue<pair<int,int>> q;
-        q.push({row,col});
         for(int i=0;i<4;i++)
         {
             int nrow=row+dx[i];
@@ -25,16 +23,12 @@ public:
         vector<vector<int>> vis(n,vector<int>(m,0));
         for(int i=0;i<m;i++)
         {
-            //vis[0][i]=1;
             if(!vis[0][i] && grid[0][i]==1)bfs(vis,grid,0,i);
-            //vis[m-1][i]=1;
             if(!vis[n-1][i] && grid[n-1][i]==1)bfs(vis,grid,n-1,i);
         }
         for(int i=0;i<n;i++)
         {
-            //vis[i][0]=1;
             if(!vis[i][0] && grid[i][0]==1)bfs(vis,grid,i,0);
-            //vis[i][n-1]=1;
             if(!vis[i][m-1] && grid[i][m-1]==1)bfs(vis,grid,i,m-1);
         }
         int ans=0;
@@ -42,7 +36,7 @@ public:
         {
             for(int j=1;j<m;j++)
             {
-                if(grid[i][j]==1 && !vis[i][j])ans++;          
+                if(!vis[i][j] && grid[i][j]==1)ans++;          
             }
         }
         return ans;
