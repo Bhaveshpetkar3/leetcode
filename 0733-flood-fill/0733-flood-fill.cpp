6756dee5a8b524a044f54dae6e0db40d7f17355d
Bhaveshpetkar3 <77126230+Bhaveshpetkar3@users.noreply.github.com>
2023-03-15 15:26:13 +0530

    Time: 9 ms (68.41%), Space: 14.6 MB (8.90%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -9,25 +9,24 @@ public:
         q.push({row,col});
         int n=image.size();
         int m=image[0].size();
+        int dx[]={-1,0,1,0};
+        int dy[]={0,-1,0,1};
         while(!q.empty())
         {
             int row=q.front().first;
             int col=q.front().second;
             q.pop();
-            for(int delrow=-1;delrow<=1;delrow++)
+            for(int i=0;i<4;i++)
             {
-                for(int delcol=-1;delcol<=1;delcol++)
-                {
-                    if((delrow==-1 || delrow==1) && delcol!=0)continue;
-                    int nrow=row+delrow;
-                    int ncol=col+delcol;
+                    //if((delrow==-1 || delrow==1) && delcol!=0)continue;
+                    int nrow=row+dx[i];
+                    int ncol=col+dy[i];
                     if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && image[nrow][ncol]==ogcol)
                     {
                         image[nrow][ncol]=color;
                         q.push({nrow,ncol});
                         vis[nrow][ncol]=1;
                     }
-                }
             }
         }
     }
