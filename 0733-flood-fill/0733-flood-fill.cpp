8ca6e2fe29f975091b4c2150faffd9e486ccc059
Bhaveshpetkar3 <77126230+Bhaveshpetkar3@users.noreply.github.com>
2023-03-15 15:40:57 +0530

    Time: 7 ms (88.53%), Space: 14.3 MB (28.66%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,40 +1,28 @@
 class Solution {
 public:
-    void bfs(vector<vector<int>> &vis,int row,int col,vector<vector<int>> &image,int color)
+    void dfs(vector<vector<int>> &vis,int row,int col,vector<vector<int>> &image,int color,int ogcolor)
     {
-        vis[row][col]=1;
-        int ogcol=image[row][col];
-        image[row][col]=color;
-        queue<pair<int,int>> q;
-        q.push({row,col});
         int n=image.size();
         int m=image[0].size();
+        vis[row][col]=1;
+        if(image[row][col]==ogcolor)image[row][col]=color;
         int dx[]={-1,0,1,0};
         int dy[]={0,-1,0,1};
-        while(!q.empty())
+        for(int i=0;i<4;i++)
         {
-            int row=q.front().first;
-            int col=q.front().second;
-            q.pop();
-            for(int i=0;i<4;i++)
-            {
-                    //if((delrow==-1 || delrow==1) && delcol!=0)continue;
-                    int nrow=row+dx[i];
-                    int ncol=col+dy[i];
-                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && image[nrow][ncol]==ogcol)
-                    {
-                        image[nrow][ncol]=color;
-                        q.push({nrow,ncol});
-                        vis[nrow][ncol]=1;
-                    }
-            }
+            int nrow=row+dx[i];
+            int ncol=col+dy[i];
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && image[nrow][ncol]==ogcolor)
+                    dfs(vis,nrow,ncol,image,color,ogcolor);
         }
+        
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int n=image.size();
         int m=image[0].size();
         vector<vector<int>>vis(n,vector<int>(m,0));
-        bfs(vis,sr,sc,image,color);
+        int ogcolor=image[sr][sc];
+        dfs(vis,sr,sc,image,color,ogcolor);
         return image;
     }
 };
