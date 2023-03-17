1ead80f66fe017ca5a9052d21e8a3df9db03dfca
Bhaveshpetkar3 <77126230+Bhaveshpetkar3@users.noreply.github.com>
2023-03-17 12:19:44 +0530

    Time: 32 ms (21.21%), Space: 13.7 MB (71.58%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>& pre) {
+        int V=n;
+        vector<int> adj[V];
+        vector<int> indegree(n,0);
+        vector<int> ans;
+        for(auto i: pre)
+        {
+            indegree[i[0]]++;
+            adj[i[1]].push_back(i[0]);
+        }
+        queue<int> q;
+		for (int i = 0; i < V; i++) {
+			if (indegree[i] == 0) {
+				q.push(i);
+			}
+		}
+		vector<int> topo;
+		while (!q.empty()) {
+			int node = q.front();
+			q.pop();
+			topo.push_back(node);
+
+			for (auto it : adj[node]) {
+				indegree[it]--;
+				if (indegree[it] == 0) q.push(it);
+			}
+		}
+        if(topo.size()!=n)return 0;
+        return 1;
+		//return topo;
+    }
+};
