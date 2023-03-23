4b47b1d6d85dadfb939cf3e7f9d33574359ea375
Bhaveshpetkar3 <77126230+Bhaveshpetkar3@users.noreply.github.com>
2023-03-23 15:10:02 +0530

    Time: 170 ms (63.71%), Space: 107.6 MB (51.49%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     long long zeroFilledSubarray(vector<int>& nums) {
         long long ans=0,cnt=0;
-        for(auto i:nums)
+        for(auto &i:nums)
         {
             if(i==0)cnt++;
             else cnt=0;
