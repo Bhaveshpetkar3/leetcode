27327353e9f5290ff4a9c408adb0b7844927af63
Bhavesh Petkar <77126230+Bhaveshpetkar3@users.noreply.github.com>
2023-07-22 19:58:34 +0530

    Time: 4 ms (56.65%), Space: 7.8 MB (61.35%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -9,10 +9,10 @@ public:
         return dp[r][c]=(solve(m,n,r-1,c,dp,grid)+solve(m,n,r,c-1,dp,grid));
     }
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        if(grid[0][0]==1)return 0;
         int m=grid.size();
         int n=grid[0].size();
         vector<vector<int>> dp(m,vector<int>(n,-1));
-        if(grid[0][0]==1)return 0;
         dp[0][0]=1;
         return solve(m,n,m-1,n-1,dp,grid);
     }
